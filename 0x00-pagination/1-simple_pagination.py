c85901606beb40779877efc886ab9452e268eb4c
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-19 18:14:21 +0300

    task all

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,10 +5,10 @@ import math
 from typing import List, Tuple
 
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
